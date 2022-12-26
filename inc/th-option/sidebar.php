efd5d4186da157115231025fbb5b477c3a222bf7
iheeresh <heereshd@gmail.com>
2022-12-26 13:41:21 +0530

    theme tested with WordPress 6.1

--- inc/th-option/sidebar.php
+++ inc/th-option/sidebar.php
@@ -6,7 +6,7 @@
             <p>
             <b><?php _e('Pre-made premium templates','shopline'); ?> </b><br>
             <?php _e('Shopline Pro is one of the most popular eCommerce theme on WordPress.','shopline'); ?> </p>
-                <center><a class="button ztabtn" target="_blank" href="https://themehunk.com/product/shopline-pro-multipurpose-shopping-theme/"><?php _e('Upgrade','shopline'); ?> </a></center>
+                <center><a class="button ztabtn" target="_blank" href="<?php echo esc_url('https://themehunk.com/product/shopline-pro-multipurpose-shopping-theme/'); ?>"><?php _e('Upgrade','shopline'); ?> </a></center>
             </div>
             <hr>
             <div class="section">
@@ -20,13 +20,13 @@
             <div class="section">
                 <h3><?php _e('Video Tutorials','shopline'); ?></h3>
                 <p><?php _e('Want a guide? We have video tutorials to walk you through getting started.','shopline'); ?> </p>
-                <a href="https://www.youtube.com/watch?v=ZrsfMyhsa5I&list=PLe42R8TvpeK1cYLccWdUB4I0oMOYgIGJf" target="_blank" class="sidebar-link"><?php _e('Watch Videos','shopline'); ?></a>
+                <a href="<?php echo esc_url('https://www.youtube.com/watch?v=ZrsfMyhsa5I&list=PLe42R8TvpeK1cYLccWdUB4I0oMOYgIGJf'); ?>" target="_blank" class="sidebar-link"><?php _e('Watch Videos','shopline'); ?></a>
             </div>
             <hr>
 
             <div class="section">
                 <h3><?php _e('Support','shopline'); ?> </h3>
                 <p><?php _e('Have a question, we are happy to help! Get in touch with our support team.','shopline'); ?></p>
-                <a href="https://themehunk.com/contact-us/" target="_blank" class="sidebar-link"><?php _e('Submit a Ticket','shopline'); ?></a>
+                <a href="<?php echo esc_url('https://themehunk.com/contact-us/'); ?>" target="_blank" class="sidebar-link"><?php _e('Submit a Ticket','shopline'); ?></a>
             </div>
         </div>
